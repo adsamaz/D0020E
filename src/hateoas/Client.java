1dfa1a6b241a9a87163f8726652c18c022fa2be5
Beldrama <roininin4@hotmail.com>
2018-02-03 13:46:36 +0100

    Made Client and server class

---
+++ src/hateoas/Client.java
@@ -0,0 +1,11 @@
+package hateoas;
+
+/*
+ * The client receives the JSON which stores the index of available options for the client.
+ * The client parses the json and chooses a specific keyword that acts as a server request of
+ * some sort.
+ */
+
+public class Client {
+
+}
---
+++ src/hateoas/Server.java
@@ -0,0 +1,10 @@
+package hateoas;
+
+/*
+ * This is the Server which will store the status of the screwdriver and its screws.
+ * converts it's information into a JSON which the client then parses.
+ */
+
+public class Server {
+
+}
