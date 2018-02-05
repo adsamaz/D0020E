267a2fffeb26a0716e9efb7730c75ce9e2f0c31b
Beldrama <roininin4@hotmail.com>
2018-02-05 16:26:40 +0100

    A start
    Yes

--- src/hateoas/Server.java
+++ src/hateoas/Server.java
@@ -1,5 +1,8 @@
 package hateoas;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /*
  * This is the Server which will store the status of the screwdriver and its screws.
  * converts it's information into a JSON which the client then parses.
@@ -7,20 +10,34 @@ package hateoas;
 
 public class Server {
 	double screw1;
-	
+	GsonBuilder builder = new GsonBuilder();
+    Gson gson = builder.create();
+    
 	public Server (){
 		
 	}
 	
-	public void tighten(double force){
+	public void tighten(int screw, double force){
 		
 	}
 	
-	public void loosen(double force){
+	public void loosen(int screw, double force){
 			
 	}
 	
 	public double status(int screwId){
 		return 1;
+		
+        gson.toJson(albums);
 	}
+	
+
+
+}
+
+
+
+class screw {
+    public int screw;
+    public double tightened; //1-100
 }
