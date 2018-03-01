ef00080b19e416f877d5fcf7e1f4acd498929b91
Beldrama <roininin4@hotmail.com>
2018-03-01 14:59:55 +0100

    With the new JDK version Gson and httpserver conflicts with each other

---
+++ src/hateoas2/Handler.java
@@ -0,0 +1,25 @@
+package hateoas2;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+ class Handler implements HttpHandler {
+	 String json;
+	 
+	 public Handler(String json) {
+		 this.json = json;
+	 }
+	 
+    public void handle(HttpExchange ex) throws IOException {        
+        ex.sendResponseHeaders(200, json.getBytes(Charset.forName("UTF-8")).length);
+        OutputStream os = ex.getResponseBody();
+        os.write(json.getBytes(Charset.forName("UTF-8")));
+        os.close();
+    }
+    
+}
--- src/hateoas2/Server.java
+++ src/hateoas2/Server.java
@@ -4,10 +4,14 @@ import org.springframework.hateoas.*;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.sun.net.httpserver.HttpServer;
 
 import java.lang.reflect.Modifier;
+import java.net.InetSocketAddress;
 
 import static java.lang.reflect.Modifier.*;
+
+import java.io.IOException;
 
 public class Server extends ResourceSupport {
 
@@ -16,10 +20,22 @@ public class Server extends ResourceSupport {
     private static Link link = new Link("/");
     private transient Screws screws;
     
-
+    HttpServer httpserver;
     
 
     public Server (){
+    	
+    	 
+    	
+    	try {
+    		httpserver = HttpServer.create(new InetSocketAddress("localhost", 7), 0);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+	
+    	
+
+    	
         this.add(new Link("/"));
         this.add(Screws.getLink());
 
@@ -27,18 +43,20 @@ public class Server extends ResourceSupport {
 
         this.screws = new Screws(5, link);
         
-        System.out.println(gson.toJson(this));
-        System.out.println();
-        System.out.println(gson.toJson(screws));
-        System.out.println();
+        System.out.println(screws.getScrew(1).getLink().getHref());
+        //System.out.println(gson.toJson(screws));
         //System.out.println(gson.toJson(screws.getScrew(2)));
         //screws.getScrew(2).tighten();
         //System.out.println(gson.toJson(screws.getStatus()));
         //System.out.println(gson.toJson(screws.getScrew(2)));
         //screws.getScrew(2).loosen();
-        System.out.println(gson.toJson(screws.getScrew(2)));
+        //System.out.println(gson.toJson(screws.getScrew(2)));
+    	System.out.println(gson.toJson(this));
 
+        //createPaths();
 
+        //httpserver.setExecutor(null); //default executor	
+        //httpserver.start();
     }
 
     public static void main(String[] args) {
@@ -49,5 +67,15 @@ public class Server extends ResourceSupport {
         this.builder = new GsonBuilder();
         this.builder.setPrettyPrinting();
         this.gson = this.builder.create();
+    }
+    
+    private void createContext(String path, String json) {
+    	httpserver.createContext(path, new Handler(json));
+    }
+    
+    private void createPaths() {
+    	System.out.println(gson.toJson(this));
+    	//createContext(this.link.getHref(), gson.toJson(this));
+    	//createContext("/second", "StartPoint"); 
     }
 }
